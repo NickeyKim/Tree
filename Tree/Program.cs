5b75cdc2daff2ea197c668248ec24ca7b34d3358
NickeyKim <nakhoekim@NAKHOEs-MacBook-Pro.local>
2015-12-12 16:30:17 +0900

    Tree
    Binary Search Tree - find node

--- Tree/Program.cs
+++ Tree/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+/*Making Binary Tree*/
 namespace Tree
 {
 	public class Node
@@ -17,6 +18,46 @@ namespace Tree
 		public Node getLeft() {return left;}
 		public Node getRight() {return right;}
 		public int getValue() {return value;}
+		/* Lookup Binary Search Tree 
+		->Compare the value with Current Node
+		Starts at the head of Tree
+		while CurVal != Null
+		if curVal = Value, return curNode;
+		else if curNode < Value, rightchild is to be CurNode
+		else if curNode > Value, leftchild is to be CurNode
+		if failed to find, return root
+		*/
+		public Node findNode(Node root, int value)
+		{
+			while (root != null) {
+				int curVal = root.getValue ();
+				if (curVal == value)
+					break;
+				else if (curVal < value) {
+					root = root.getRight ();
+				} 
+				else if (curVal > value) {
+					root = root.getLeft ();
+				}
+			}
+			return root;
+		}
+		/* Making Recursively find Node in Binary search Tree */
+		public Node findRecur(Node root, int value)
+		{
+			if (root == null)
+				return null;
+			else {
+				int curVal = root.getValue ();
+				if (curVal == value)
+					return root;
+				else if (curVal < value) {
+					return findRecur (root.getRight (), value);
+				} else {
+					return findRecur (root.getLeft (), value);
+				}
+			}
+		}
 
 		public static void Main (string[] args)
 		{
@@ -28,10 +69,17 @@ namespace Tree
 			Node b = new Node (d, e, 3);
 			Node c = new Node (f, g, 10);
 			Node a = new Node (b, c, 5);
-			Node y = a.getRight();
-			int x = y.getValue ();
-			Console.WriteLine ("Value:"+ x);
-		
+
+			Node find = new Node (null, null, 0);
+			find = find.findRecur (a, 0);
+			if (find != null) {
+				Console.WriteLine ("Value:" + find.getValue () + " found");
+			} else {
+				
+				Console.WriteLine ("Not Found");
+			}
+
+
 			Console.WriteLine ("Hello World!");
 		}
 	}
